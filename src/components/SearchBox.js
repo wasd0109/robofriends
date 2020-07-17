eaf0105c20a62affc05b786b3ef7142d07220e70
Ken Cheung <wasd0109.dev@gmail.com>
2020-07-17 23:23:51 +0800

    Optimize for lighthouse

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,6 +3,7 @@ import React from 'react';
 const SearchBox = ({ searchChange }) => {
   return (
     <div className="pa2">
+      <label htmlFor="search"></label>
       <input
         className="pa3 ba b--green bg-lightest-blue"
         type="search"
